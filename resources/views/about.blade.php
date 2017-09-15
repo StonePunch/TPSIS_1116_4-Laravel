a166d34d729aa6d4ffd1093f31a794acf274f7d3
Carlos Oliveira <klumpman7@gmail.com>
2017-09-15 23:20:58 +0100

    Site Design

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,84 @@
-<html>About Page</html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!--[if lt IE 9]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title>B-School Free Education HTML5 Website Template | WebThemez</title>
+    <meta name="description" content="">
+    <meta name="author" content="WebThemez">
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lte IE 8]>
+    <script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+    <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+    <link href="css/animate.css" rel="stylesheet" media="screen">
+    <!-- Owl Carousel Assets -->
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css" />
+    <!-- Font Awesome -->
+    <link href="font/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<section id="aboutUs">
+    <div class="container">
+        <div class="heading text-center">
+            <!-- Heading -->
+            <h2>About Us</h2>
+            <p>At lorem Ipsum available, but the majority have suffered alteration in some form by injected humour.</p>
+        </div>
+        <div class="row feature design">
+            <div class="area1 columns left">
+                <h3>Our Design tells about quality</h3>
+                <p>Lorem ipsum dolor sit amet, ea eum labitur scsstie percipitoleat fabulas complectitur deterruisset at pro. Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt.
+                    Nec et jority have suffered alteration. </p>
+                <p>Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt.
+                    Nec et amet vidisse mentitumsstie percipitoleat fabulas. </p>
+                <a href="#" class="btn">Request Quote</a>
+            </div>
+            <div class="area2 columns feature-media right"> <img src="images/about-img.jpg" alt="" width="100%"> </div>
+        </div>
+        <div class="row dataTxt">
+            <div class="col-md-4 col-sm-6">
+                <h3>Our Education</h3>
+                <p>Lorem ipsum dolor consectetursit amet, consectetur adipiscing elit consectetur euismod </p>
+                <p>Lorem ipsum dolor sit amet, ea eum labitur scsstie percipitoleat fabulas complectitur deterruisset at pro. Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea.</p>
+
+                <br>
+            </div>
+
+            <div class="col-md-4 col-sm-6">
+
+                <h3>Courses</h3>
+                <p>Lorem ipsum dolor consectetursit amet, consectetur adipiscing elit consectetur euismod </p>
+                <ul class="listArrow">
+                    <li>Lorem ipsum dolor consectetursit amet, consectet</li>
+                    <li>Has molestie percipit an. Falli volumus efficiantur</li>
+                    <li>Falli volumus efficiantur sed id, ad vel noster</li>
+                    <li>Lorem ipsum dolor consectetursit amet, consectetur</li>
+                    <li>Ius ut etiam vivendo, graeci iudicabit constitutoa</li>
+                </ul>
+                <!-- Accordion starts -->
+            </div>
+
+            <div class="col-md-4 col-sm-6">
+
+                <h3>Latest News</h3>
+                <p>Lorem ipsum dolor consectetursit amet, consectetur adipiscing elit consectetur euismod </p>
+                <ul  class="list3">
+                    <li>Lorem ipsum dolor consectetursit</li>
+                    <li>Has molestie percipit an Falli</li>
+                    <li>Falli volumus efficiantur sed id</li>
+                    <li>Lorem ipsum dolor consectetu</li>
+                </ul>
+
+                <!-- Accordion starts -->
+            </div>
+
+        </div>
+    </div>
+</section>
